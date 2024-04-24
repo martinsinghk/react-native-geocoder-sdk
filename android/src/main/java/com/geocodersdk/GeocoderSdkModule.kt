4c094d518e408c1ca4a540be00e9994ca4879ba9
Martin Wong <martin_wong@mtelnet.com>
2024-04-24 13:07:12 +0800

    feat: android reverseGeocodeLocation

--- android/src/main/java/com/geocodersdk/GeocoderSdkModule.kt
+++ android/src/main/java/com/geocodersdk/GeocoderSdkModule.kt
@@ -1,8 +1,9 @@
 package com.geocodersdk
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.*
+import android.location.Geocoder
+import java.io.IOException
+import java.util.*
 
 class GeocoderSdkModule internal constructor(context: ReactApplicationContext) :
   GeocoderSdkSpec(context) {
@@ -11,11 +12,30 @@ class GeocoderSdkModule internal constructor(context: ReactApplicationContext) :
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  override fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  override fun reverseGeocodeLocation(coordinates: ReadableMap, promise: Promise) {
+        val latitude = coordinates.getDouble("latitude")
+        val longitude = coordinates.getDouble("longitude")
+        val geocoder = Geocoder(reactApplicationContext, Locale.getDefault())
+
+        try {
+            val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+            if (!addresses.isNullOrEmpty()) {
+                val address = addresses[0]
+
+                val writableMap = WritableNativeMap()
+                writableMap.putString("addressLine", address.getAddressLine(0))
+                writableMap.putString("locality", address.locality)
+                writableMap.putString("countryName", address.countryName)
+                writableMap.putString("countryCode", address.countryCode)
+
+                promise.resolve(writableMap)
+            } else {
+                promise.reject("NOT_FOUND", "No address found for the given coordinates")
+            }
+        } catch (e: IOException) {
+            promise.reject("ERROR", e.message, e)
+        }
   }
 
   companion object {
--- android/src/oldarch/GeocoderSdkSpec.kt
+++ android/src/oldarch/GeocoderSdkSpec.kt
@@ -1,11 +1,9 @@
 package com.geocodersdk
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.*
 
 abstract class GeocoderSdkSpec internal constructor(context: ReactApplicationContext) :
   ReactContextBaseJavaModule(context) {
 
-  abstract fun multiply(a: Double, b: Double, promise: Promise)
+  abstract fun reverseGeocodeLocation(coordinates: ReadableMap, promise: Promise)
 }
